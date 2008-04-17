ad245a4df4f4b1c26223808333c359cd6e842991
Sylvain Baubeau <bob@glumol.com>
2008-04-17 06:49:01 +0000

    Copyright and license added
    git-svn-id: http://bob.alpha.agorabox.org/repos/glumol/trunk/poujol@280 29d5ca58-9003-0410-8356-a7fefff29482

--- console.h
+++ console.h
@@ -1,3 +1,23 @@
+/*
+Glumol - An adventure game creator
+Copyright (C) 1998-2008  Sylvain Baubeau & Alexis Contour
+
+This file is part of Glumol.
+
+Glumol is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Glumol is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Glumol.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef __CONSOLE_H__#define __CONSOLE_H__#include <ClanLib/core.h>#include <ClanLib/display.h>
 /*struct sconsole{	bool bexist;	const char* title;	CL_Sizef size;	CL_ConsoleWindow* h_console;};class console_stdout{public:	sconsole console;	console_stdout();	~console_stdout();	void redirect_std();	void CreateConsole();	void End();	void write(const char* msg);
 };*/#endif
